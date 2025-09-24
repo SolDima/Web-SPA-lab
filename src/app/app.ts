d53bf7ff8246ca032e59b3ec0014e00ae8be35cf
Dima <dimaa17.fl@gmail.com>
2025-09-24 13:49:00 +0300

    pract 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Layout } from './layout/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Layout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('MySportApp');
+  protected readonly title = 'Angular';
 }
