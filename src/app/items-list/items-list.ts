2ab4352febae3cb0cfaac4bd9964f582600ea796
Dima <dimaa17.fl@gmail.com>
2025-09-26 21:28:02 +0300

    pr2_nopage_noerror

--- src/app/items-list/items-list.ts
+++ src/app/items-list/items-list.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Item } from '../shared/models/item.model';
 import { items } from '../data/items/items';
+import { ItemsCardComponent } from '../items-card/items-card';
 
 @Component({
   selector: 'app-items-list',
-  templateUrl: './items-list.html'
+  templateUrl: './items-list.html',
+  imports: [ItemsCardComponent],
 })
 export class ItemsListComponent {
   items: Item[] = items;
