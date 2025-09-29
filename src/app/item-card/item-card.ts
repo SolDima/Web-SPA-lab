75fcc03093a14aef3ad73f24de0170f69306140f
Dima <dimaa17.fl@gmail.com>
2025-09-29 13:00:09 +0300

    lab 3

--- src/app/item-card/item-card.ts
+++ src/app/item-card/item-card.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { item } from '../shared/models/item.model';
-import { NgOptimizedImage } from '@angular/common';
+import {NgOptimizedImage, NgIf, NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-item-card',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, NgIf, NgClass],
   templateUrl: './item-card.html',
+  styleUrls: ['./item-card.css']
 })
 export class ItemCard {
   @Input() card_of_item!: item;
+  showDetails :boolean = false;
 }
--- src/app/items-list/items-list.ts
+++ src/app/items-list/items-list.ts
@@ -8,11 +8,12 @@ import { NgFor } from '@angular/common';
   selector: 'app-items-list',
   imports: [ItemCard, NgFor],
   templateUrl: './items-list.html',
+  styleUrls: ['./items-list.css'],
 })
 export class ItemsList {
 
   // Масив товарів
-  ItemsList: item[] = [
+  itemslist: item[] = [
     {
       id: 1,
       name: "Гантеля 2кг",
@@ -29,6 +30,27 @@ export class ItemsList {
     },
     {
       id: 3,
+      name: "Гантелі регульовані",
+      image: "https://images.prom.ua/6199802815_w1280_h640_6199802815.jpg",
+      description: "Щоб звіряти вагу кавуна на базарі",
+      awgPrice: 1000
+    },
+    {
+      id: 4,
+      name: "Гантеля 2кг",
+      image: "https://stok.top/image/cache/catalog/products/ganteli/sport/ganteli-dlya-fitnesu-2sht-po-1-5kg-perto-00887-blakitni-00887-700x700.jpg",
+      description: "Щоб був біцепс як у Арнольда",
+      awgPrice: 100
+    },
+    {
+      id: 5,
+      name: "Бігова доріжка",
+      image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcontent2.rozetka.com.ua%2Fgoods%2Fimages%2Fbig%2F302449882.jpg&f=1&nofb=1&ipt=cd13fbce40886f1256c7f40a8f0aeb2e8c67305d9424ef27a464bb97de24f232",
+      description: "Щоб бігати, не виходячи з хати",
+      awgPrice: 1000
+    },
+    {
+      id: 6,
       name: "Гантелі регульовані",
       image: "https://images.prom.ua/6199802815_w1280_h640_6199802815.jpg",
       description: "Щоб звіряти вагу кавуна на базарі",
